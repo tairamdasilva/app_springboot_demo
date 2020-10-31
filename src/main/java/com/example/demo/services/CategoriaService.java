01d059d393b51cec01f0ffb18d58280ca3bd31e9
tairamdasilva <italo.tairan@hotmail.com>
2020-10-31 17:26:20 -0300

    create categoria repository and service

---
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -0,0 +1,34 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	//deprecated because version is JAVA 8 (spring vesion 2)
+//	public Categoria buscar(Integer id) {
+//		
+//		Categoria obj = repo.findOne(id);
+//		
+//		return obj;
+	
+	
+	
+		//using findById(...);
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		} 
+	
+		
+	}
+
