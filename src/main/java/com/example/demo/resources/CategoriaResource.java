5cff7132c2501d8db376b9ac882d5181b3cbb1e0
tairamdasilva <italo.tairan@hotmail.com>
2020-10-31 16:13:25 -0300

    testing first class Categoria

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
 
 @RestController
 @RequestMapping (value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST OK!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Office");
+		
+		
+		List<Categoria> lista = new ArrayList();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 	
